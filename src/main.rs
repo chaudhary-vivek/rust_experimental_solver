80277fb24a2d98ae84bffdebe9a2fdda53c40084
Vivek Chaudhary <48665832+chaudhary-vivek@users.noreply.github.com>
2024-12-10 21:41:28 +0530

    added rust file for solver

---
+++ src/main.rs
@@ -0,0 +1,24 @@
+fn add_numbers(x:i32, y:i32) -> i32{
+    x + y
+}
+
+fn nice(x:i32){
+    for _ in 0..x{
+        println!("nice!");
+    }
+}
+
+fn nice_plus(x:i32){
+    for x in 1..x{
+        println!("{}", x);
+    }
+}
+
+fn main() {
+    let result = add_numbers(340, 9);
+    println!("Hello, world!");
+    println!("The sum is {}", result);
+    nice(3);
+    nice_plus(3);
+
+}
